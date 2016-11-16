b52a857a2e6aff9ea40dc72c79b3ed1d710bf94c
Stephanie Lane <email": null>
2016-11-16 17:48:17 -0600

    added association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base
 	has_secure_password
+
+	has_many :albums
+
 	validates :first_name, :last_name, presence: true
 	validates :username, :email, presence: true, uniqueness: true
 
 end 
-
