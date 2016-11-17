87d0b0aba583786e8f13ebdbed12a650f21c6721
Stephanie Lane <email": null>
2016-11-17 11:08:32 -0600

    added 'end'

--- spec/features/visiting_the_registration_page_feature_spec.rb
+++ spec/features/visiting_the_registration_page_feature_spec.rb
@@ -3,8 +3,13 @@ require 'rails_helper'
 feature "visiting the registration page" do 
 	context "when new user registers" do 
 	
+	new_user = User.create!(first_name: "foo", last_name: "bar", username: "foobar", email: "foo@bar.com", password: "password")
+
 	visit "/users/new"
-	
+
+	within(".form-group")
+	end 
+
 	
 	end 
 
