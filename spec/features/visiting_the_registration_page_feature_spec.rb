dc37a49fe499fb7c8147fab9e1a145139d0f2b15
Stephanie Lane <email": null>
2016-11-17 11:21:31 -0600

    removed rspec test for now

--- spec/features/visiting_the_registration_page_feature_spec.rb
+++ spec/features/visiting_the_registration_page_feature_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-feature "visiting the registration page" do 
-	context "when new user registers" do 
+# feature "visiting the registration page" do 
+# 	context "when new user registers" do 
 	
-	new_user = User.create!(first_name: "foo", last_name: "bar", username: "foobar", email: "foo@bar.com", password: "password")
+# 	new_user = User.create!(first_name: "foo", last_name: "bar", username: "foobar", email: "foo@bar.com", password: "password")
 
-	visit "/users/new"
+# 	visit "/users/new"
 
-	within(".form-group")
-	end 
+# 	within(".form-group")
+# 	end 
 
 	
-	end 
+# 	end 
 
-end 
+# end 
