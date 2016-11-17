ec50f00f3546d56d53f30127a44da26521401aeb
Corbin Via <email": null>
2016-11-16 18:01:56 -0600

    Add foreign key field to pictures, add other commands to travis file

--- db/migrate/20161116225935_create_pictures.rb
+++ db/migrate/20161116225935_create_pictures.rb
@@ -2,6 +2,7 @@ class CreatePictures < ActiveRecord::Migration[5.0]
   def change
     create_table :pictures do |t|
       t.attachment :image
+      t.integer :album_id
 
       t.timestamps
     end
