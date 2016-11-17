256142171e91ed8f2866e419d78a6b1f6cd0e619
Stephanie Lane <email": null>
2016-11-17 13:10:28 -0600

    moved visiting homepage to features, registration page tests written

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "sessions#new"
 
   get 'users/new' => "users#new"
 
@@ -11,6 +12,5 @@ Rails.application.routes.draw do
 
   get '/sessions' => "sessions#new"
 
-  root "sessions#new"
 
 end
--- spec/features/visiting_the_registration_page_feature_spec.rb
+++ spec/features/visiting_the_registration_page_feature_spec.rb
@@ -1,16 +1,27 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# feature "visiting the registration page" do 
-# 	context "when new user registers" do 
+feature "visiting the registration page" do 
 	
-# 	new_user = User.create!(first_name: "foo", last_name: "bar", username: "foobar", email: "foo@bar.com", password: "password")
+	context "when new user registers" do 
+		new_user = User.create!(first_name: "foo", last_name: "bar", username: "foobar", email: "foo@bar.com", password: "password")
 
-# 	visit "/users/new"
+		visit "/users/new"
 
-# 	within(".form-group")
-# 	end 
+		click_button 'Create User'
 
-	
-# 	end 
+		expect(page).to have_current_path user_path(new_user)
 
-# end 
+	end 
+
+	context "when user registration fails" do 
+		new_user = User.create!(first_name: "foo", last_name: "bar", email: "foo@bar.com", password: "password")
+
+		visit "/users/new"
+
+		click_button 'Create User'
+
+		expect(page).to have_text("can't be blank")
+
+	end 
+
+end 
