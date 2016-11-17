adafd2d080dd8d6956290baa8cb84bb6d316ac36
Stephanie Lane <email": null>
2016-11-17 15:49:47 -0600

    nested shallow routes with resources added in

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,12 @@ Rails.application.routes.draw do
 
   root "sessions#new"
 
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show] do 
+  	resources :albums, shallow: true do 
+  		resources :photos, shallow: true
+  	end 
+  end 
 
-  get 'users/:id/albums/new' => "albums#new", as: 'users_albums_new'
-
-  post '/sessions' => "sessions#create"
-
-  get '/sessions' => "sessions#new"
-
-  post '/sessions' => "sessions#create"
+resources :sessions, only: [:new, :create, :destroy ]
 
 end
