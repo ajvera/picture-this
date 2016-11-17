513752499e7a31b692844c5c4191432ac989ee78
Stephanie Lane <email": null>
2016-11-17 15:14:27 -0600

    fixed merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   root "sessions#new"
 
-<<<<<<< HEAD
   resources :users, only: [:new, :create, :show]
-=======
+
   get 'users/:id/albums/new' => "albums#new", as: 'users_albums_new'
 
   post '/sessions' => "sessions#create"
->>>>>>> 2bf02a38ea64cc51bf7d3fb346417d9fc3216a77
 
   get '/sessions' => "sessions#new"
 
