437fd92465afdfc1a6c84a9985fc939125db7182
Corbin Via <email": null>
2016-11-17 14:31:15 -0600

    Add tests for user profile page

--- spec/features/user_profile_page_spec.rb
+++ spec/features/user_profile_page_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 feature "visiting the profile page" do
   scenario "the user is on the profile page" do
-    new_user = User.create!(first_name: "bloop", last_name: "bleep", username: "blop", email: "blop@blop.com", password: "hmm")
-
+  new_user = User.create!(first_name: "bloop", last_name: "bleep", username: "blup", email: "me@me.com", password: "hmm")
     visit "/users/#{new_user.id}"
+    expect(page).to have_content("Welcome #{new_user.username}!")
+  end
 
-    expect(page).to have_content("Welcome")
+  scenario "the user is on the profile page" do
+    new_user = User.create!(first_name: "bloop", last_name: "bleep", username: "blup", email: "me@me.com", password: "hmm")
+    visit "/users/#{new_user.id}"
+    expect(page).to have_content("Your albums")
+  end
+
+  scenario "the user is on the profile page" do
+    new_user = User.create!(first_name: "bloop", last_name: "bleep", username: "blup", email: "me@me.com", password: "hmm")
+    visit "/users/#{new_user.id}"
+    expect(page).to have_content("Create new album")
   end
 
 end
