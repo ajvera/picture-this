14f6ed62e5ed50d8a26b3b72000475738cfa8975
Matt Grimm <m.m.grimm1@gmail.com>
2016-11-17 12:18:06 -0600

    Create navbar and include session helpers

---
+++ app/helpers/session_helpers.rb
@@ -0,0 +1,14 @@
+helpers do
+
+  def logged_in?
+    session[:user_id] != nil
+  end
+
+  def current_user
+    @_cached_user ||= User.find(session[:user_id]) if logged_in?
+
+  def logged_in_user?(@user)
+    @user == current_user
+  end
+
+end
