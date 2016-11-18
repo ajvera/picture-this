f590d2bcdd33f4e0d8a4ab54d0f674398dc0d326
Stephanie Lane <email": null>
2016-11-18 14:07:58 -0600

    stuff

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,15 +4,14 @@ class Picture < ActiveRecord::Base
   has_attached_file :image, styles: { 
   	small: "64x64", 
   	med: "200x200", 
-  	large: "400x400" },
-  	:storage => :s3,
-    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+  	large: "400x400" }
+  	# :storage => :s3,
+   #  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
 
-  def s3_credentials
-    {:bucket => , :access_key_id => , :secret_access_key => , :s3_region => }
-  end
+  # def s3_credentials
+  #   {:bucket => ENV["S3_BUCKET_NAME"], :access_key_id => ENV["AWS_ACCESS_KEY_ID "], :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"], :s3_region => "us-east-1" }
+  # end
 
   validates_attachment :image, presence: true,
     content_type: { content_type: "image/jpeg" }
-    #,size: { in: 0..20.kilobytes }
 end
